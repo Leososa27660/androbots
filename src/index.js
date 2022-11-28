d6cb720a60f620bee03a39968932157a93f76d56
leonardososa <leonardo_sosa2018@hotmail.com>
2022-11-28 08:47:53 -0300

    json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  HashRouter,
-} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.jsx';
 import App from './App';
 
 
@@ -22,49 +14,3 @@ root.render(
 );
 
 
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <HashRouter basename="/">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </HashRouter>
-    </BrowserRouter>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
